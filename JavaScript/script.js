259936fef8703dc1b28f3e85fa0e0f2dbf45c186
Oosz5Urae <106632294+Oosz5Urae@users.noreply.github.com>
2022-09-15 05:59:52 -0400

    Por buen camino

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,8 @@
-dos
+let add = document.querySelector("#add");
+
+add.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number (output.innerText) + 1;
+
+  output.innerText = result;
+});
